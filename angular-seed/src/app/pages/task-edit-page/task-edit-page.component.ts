0c77eab661c6fd42d9df4a478feda80ebe4f1780
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2018-08-15 21:07:43 -0500

    solve

--- angular-seed/src/app/pages/task-edit-page/task-edit-page.component.ts
+++ angular-seed/src/app/pages/task-edit-page/task-edit-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import {Todo} from "../../models/todo";
+import {TodoService} from "../../services/todo.service";
 
 @Component({
   selector: 'app-task-edit-page',
@@ -6,13 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-edit-page.component.css']
 })
 export class TaskEditPageComponent implements OnInit {
+
+  private todoForm;
+
   constructor(
-  ) {
+      public formBuilder: FormBuilder,
+      public todoService: TodoService,
+      public router: Router) {
 
   }
 
   ngOnInit() {
-
+    this.todoForm = this.formBuilder.group({
+        description: '',
+        completed: '',
+        priority: ''
+      });
   }
+
+    onSubmit() {
+        this.todoService.create(
+            this.todoForm.get('description').value,
+            this.todoForm.get('priority').value,
+            Boolean(this.todoForm.get('completed').value)
+        );
+
+        this.router.navigate(['/tasks']);
+    }
 
 }
